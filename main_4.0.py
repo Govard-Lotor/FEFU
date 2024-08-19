4885241ad5edd7b36ce6ea447280fc162577f3c3
Ivan <2016.skywalk@gmail.com>
2024-08-19 16:09:02 +1000

    final_2.0

--- main_4.0.py
+++ main_4.0.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import pygame
 import sys
+import os
 
 
 class Model:
@@ -304,9 +305,6 @@ class Model:
             self.energy_all_change, self.energy_all = 0, 0
             self.count_energy_all()
 
-    def checker_file(self):
-        pass
-
     def start(self):
         self.create_spin_matrix()
         self.create_connection_matrix()
@@ -346,6 +344,41 @@ class Model:
         self.algorithm_1()
         self.count_frustration_change()
         self.count_energy_all()
+
+    def check_files(self, file_manager, size, size_matrix, connection_matrix_extra):
+        self.size = size
+        self.size_matrix = size_matrix
+
+        letter = []
+
+        for matrix in connection_matrix_extra:
+            self.matrix_connection = file_manager.create_connection_matrix(connection_matrix_extra[matrix])
+
+            self.create_spin_matrix()
+            self.create_spin_id()
+            self.create_quartet()
+
+            self.count_frustration()
+
+            self.algorithm_1()
+
+            self.count_frustration_change()
+            self.count_energy_all()
+            #######################
+            with open(f"files/{matrix}/{file_manager.res_files[matrix]}", 'r') as f:
+                for i in range(4):
+                    f.readline()
+                energy_original = f.readline()
+
+
+            message = \
+                f"{file_manager.con_files[matrix]} \t energy_start - {self.energy_all} \t|\t " \
+                f"energy_final - {self.energy_all_change} \t energy_original - {energy_original.split()[1]}\n"
+            letter.append(message)
+
+            with open("results.txt", "w+") as file:
+                file.writelines(letter)
+                file.close()
 
     def return_parameters(self):
         supper_array = [self.size, self.size_matrix, self.matrix_spin, self.matrix_id, self.matrix_connection,
@@ -401,6 +434,9 @@ class Interface:
         self.frustration_col_ch = frustration_col_ch
         self.energy_all_change = energy_all_change
         self.matrix_spin_change = matrix_spin_change
+
+    def connect(self, model):
+        self.model = model
 
     def draw_spin(self, matrix, surface):
         x, y = 6, 6
@@ -538,19 +574,19 @@ class Interface:
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 3:
-                        model.clear()
-                        model.start()
-                        self.restart(*model.return_parameters())
+                        self.model.clear()
+                        self.model.start()
+                        self.restart(*self.model.return_parameters())
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 4:
-                        model.algorith_2()
-                        self.restart(*model.return_parameters())
+                        self.model.algorith_2()
+                        self.restart(*self.model.return_parameters())
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 5:
-                        model.algorithm_3()
-                        self.restart(*model.return_parameters())
+                        self.model.algorithm_3()
+                        self.restart(*self.model.return_parameters())
 
                 ######################################
                 self.draw_surface(self.frustration_row, self.frustration_col, self.surface,
@@ -564,12 +600,230 @@ class Interface:
                 pygame.display.flip()
 
 
-size = 20
-model = Model(size)
-model.start()
+class File_manager:
+    def __init__(self):
+        self.directories = []
+        self.con_files = {}
+        self.res_files = {}
+
+        self.connection_matrix_extra = {}
+
+        self.matrix_connection = []
+
+        self.size = 0
+        self.size_matrix = 0
+
+    def find_dirs(self):
+        for filename in os.listdir("files"):
+            self.directories.append(filename)
+
+    def create_connection_directories(self):
+        if self.directories == 0:
+            return 0
+
+        for dir in self.directories:
+
+            for file in os.listdir(f"files/{dir}"):
+                if "cell" in file:
+                    self.con_files[f"{dir}"] = file
+                if "gem" in file:
+                    self.res_files[f"{dir}"] = file
+
+    def find_size(self):
+        a = ''
+        for file in os.listdir(f'files/{self.directories[0]}'):
+            if "gem" in file:
+                a = file
+                break
+        with open(f'files/{self.directories[0]}/{a}', "r") as file:
+            for line in file:
+                self.size = int(line)
+                self.size_matrix = self.size * 2 - 1
+                break
+            file.close()
+
+    def create_connection_matrix_extra(self):
+        for one in self.con_files:
+            way = f'files/{one}/{self.con_files[one]}'
+            with open(way, "r") as file:
+                self.connection_matrix_extra[one] = np.array(file.readlines())
+                file.close()
+
+        for cell in self.connection_matrix_extra:
+            self.connection_matrix_extra[cell] = \
+                np.array(list(map(list, (map(int, (i[0:-2]).split()) for i in self.connection_matrix_extra[cell]))))
+
+    def find_x_y(self, number):
+        y = number // self.size
+        x = number % self.size
+        return x, y
+
+    def return_number(self, x, y):
+        return y * self.size + x
+
+    def create_connection_matrix(self, connection_matrix_extra):
+        a = 0
+        self.matrix_connection = np.random.choice([0], (self.size_matrix, self.size_matrix))
+        for i in range(self.size_matrix):
+            for j in range(self.size_matrix):
+                if i % 2 == 0:
+                    if j % 2 == 0:
+                        self.matrix_connection[i][j] = a
+                        a += 1
+                else:
+                    if j % 2 != 0:
+                        self.matrix_connection[i][j] = -5
+
+        counter = 0
+        for line in range(0, self.size_matrix - 1, 2):
+            for spin in range(0, self.size_matrix - 1, 2):
+
+                self.matrix_connection[line][spin + 1] = connection_matrix_extra[counter][counter + 1]
+
+                # x, y = self.find_x_y(spin)
+                # a = self.return_number(x, y + 1)
+                self.matrix_connection[line + 1][spin] = connection_matrix_extra[counter][counter + 8]
+
+                if spin % (self.size - 2) == 0:
+                    self.matrix_connection[line + 1][spin + 2] = connection_matrix_extra[counter + 1][counter + 9]
+                counter += 1
+            counter += 1
+
+        for spin in range(0, self.size_matrix - 1, 2):
+            self.matrix_connection[self.size_matrix - 1][spin + 1] = connection_matrix_extra[counter][counter + 1]
+            counter += 1
+        self.matrix_connection = np.array(self.matrix_connection)
+        return self.matrix_connection
+
+    def start(self):
+        self.find_dirs()
+        self.create_connection_directories()
+        self.find_size()
+        self.create_connection_matrix_extra()
+
+    def return_parameters(self):
+        array = [self.size, self.size_matrix, self.connection_matrix_extra]
+        return array
 
 
-interface = Interface(*model.return_parameters())
-interface.start()
+class Model_for_file:
+    def __init__(self, size, connection_matrix):
+        self. size = size
+
+        self.quartet = []
+        self.matrix_id = []
+
+        self.connection_matrix = connection_matrix
+
+        self.matrix_spin = []
+        self.matrix_spin_change = []
+
+    def find_x_y(self, number):
+        y = number // self.size
+        x = number % self.size
+        return x, y
+
+    def return_number(self, x, y):
+        return y * self.size + x
+
+    def create_quartet(self):
+        quartet = []
+
+        dop = []
+
+        for number in range(self.size * self.size):
+            x, y = self.find_x_y(number)
+
+            if x < self.size - 1:
+                dop.append(self.return_number(x + 1, y))
+            if y < self.size - 1:
+                dop.append(self.return_number(x + 1, y + 1))
+                dop.append(self.return_number(x, y + 1))
+            if len(dop) == 3:
+                dop.append(self.return_number(x, y))
+
+            if len(dop) == 4:
+                quartet.append(dop)
+            dop = []
+
+        self.quartet = np.array(quartet)
+        # self.quartet = list(map(list, (i + 1 for i in self.quartet)))
+        # print(self.quartet)
+
+    def create_matrix_id(self):
+        dop = []
+        collector = []
+
+        for spin in range(1, self.size * self.size + 1):
+            dop.append(spin - 1)
+            if spin != 0 and spin % self.size == 0:
+                collector.append(dop)
+                dop = []
+        self.matrix_id = np.array(collector)
+        print(self.matrix_id)
+
+    def count_energy(self, one_quartet, connection_matrix, spin_matrix):
+        a, b, c, d = one_quartet[3], one_quartet[0], one_quartet[2], one_quartet[1]
+        en_ab, en_bd, en_dc, en_ac = 0, 0, 0, 0
+        energy = 0
+
+        x_a, y_a = self.find_x_y(a)
+        x_b, y_b = self.find_x_y(b)
+        x_c, y_c = self.find_x_y(c)
+        x_d, y_d = self.find_x_y(d)
+
+        spin_a = spin_matrix[y_a][x_a]
+        spin_b = spin_matrix[y_b][x_b]
+        spin_c = spin_matrix[y_c][x_c]
+        spin_d = spin_matrix[y_d][x_d]
+
+        en_ab = spin_a * spin_b * connection_matrix[a][b]
+        en_bd = spin_b * spin_d * connection_matrix[b][d]
+        en_dc = spin_c * spin_d * connection_matrix[d][c]
+        en_ac = spin_a * spin_c * connection_matrix[c][a]
+
+        energy = en_ab + en_bd + en_dc + en_ac
+        return energy
+
+    def generate_spin_matrix(self):
+        self.matrix_spin = np.random.choice([-1, 1], (self.size, self.size))
+        self.matrix_spin_change = self.matrix_spin.copy()
+
+    def spin(self, x, y, spin_matrix):
+        spin_matrix[y][x] = spin_matrix[y][x] * -1
 
 
+
+
+
+
+
+
+
+
+# f = File_manager()
+# f.find_dirs()
+# f.create_connection_directories()
+# f.find_size()
+# print(f.size, f.size_matrix)
+# f.create_connection_matrix_extra()
+# for i in f.connection_matrix_extra:
+#     f.create_connection_matrix(f.connection_matrix_extra[i])
+#     break
+
+
+# size = 20
+# model = Model(size)
+# model.start()
+#
+#
+# interface = Interface(*model.return_parameters())
+# interface.connect(model)
+# interface.start()
+
+file_manager = File_manager()
+model = Model(8)
+
+file_manager.start()
+print(file_manager.res_files)
+model.check_files(file_manager, *file_manager.return_parameters())
